a99a92df3cad628226b3f7ede1106d5c6b4def25
safal <safalgautam212@gmail.com>
2025-09-22 21:24:22 +0545

    test added

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model 
+from .models import Order
+from django.urls import reverse
+from rest_framework import status
 
+
+User = get_user_model()
 # Create your tests here.
+class UserOrderTestCase(TestCase):
+    def setUp(self):
+        user1 = User.objects.create_user(
+            username = 'user1',
+            password = 'testpass123'
+        )
+        user2 = User.objects.create_user(
+            username='user2',
+            password= 'testpass123'
+        )
+        Order.objects.create(user = user1)
+        Order.objects.create(user = user1)
+        Order.objects.create(user = user2)
+        Order.objects.create(user = user2)
+    
+    def test_user_order_endpoint_retrieves_only_authenticated_user_orders(self):
+        user = User.objects.get(username = 'user1')
+        self.client.force_login(user)
+        response = self.client.get(reverse('user-orders'))
+        
+        assert response.status_code ==status.HTTP_200_OK
+        orders = response.json()
+        self.assertTrue(all(order['user'] == user.username for order in orders))
+
+    def test_user_order_list_unauthenticated(self):
+        response = self.client.get(reverse('user-orders'))
+        self.assertEqual(response.status_code,status.HTTP_403_FORBIDDEN)
--- api/views.py
+++ api/views.py
@@ -5,40 +5,63 @@ from api.models import Product,Order, OrderItem
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from rest_framework import generics
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 
-@api_view(['GET'])
-def product_list(request):
-    products =  Product.objects.all()
-    serializer = ProductSerializer(products,many =True)
-    return Response(
-        serializer.data
-    )
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.exclude(stock__gt=0)
+    serializer_class = ProductSerializer
 
 
-@api_view(['GET'])
-def product_detail(request,pk):
-    product =  get_object_or_404(Product,pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id'
 
-
-@api_view(['GET'])
-def order_list(request):
-    orders = Order.objects.prefetch_related(
+    
+class OrderListAPIView(generics.ListAPIView):
+    queryset = Order.objects.prefetch_related(
         'items','items__product',
         ).all()
-    serializer = OrderSerializer(orders, many = True)
-    return Response(serializer.data)
+    serializer_class = OrderSerializer
+    
+    
+class UserOrderListAPIView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Order.objects.prefetch_related(
+        'items','items__product',
+        ).all()
+    serializer_class = OrderSerializer
+    
+    def get_queryset(self):
+        user = self.request.user
+        qs = super().get_queryset()
+        return qs.filter(user=user)
+    
+    
+class ProductInfoAPIView(APIView):
+    def get(self,request):
+        products = Product.objects.all()
+        serializer = ProductInfoSerializer({
+            'products':products,
+            'count': len(products),
+            'max_price': products.aggregate(max_price = Max('price'))['max_price']
+        })
+        return Response(serializer.data)
+        
 
-@api_view(['GET'])
-def product_info(request):
-    products = Product.objects.all()
-    serializer = ProductInfoSerializer({
-        'products':products,
-        'count': len(products),
-        'max_price': products.aggregate(max_price = Max('price'))['max_price']
-    })
-    return Response(serializer.data)
+
+
+# @api_view(['GET'])
+# def product_info(request):
+#     products = Product.objects.all()
+#     serializer = ProductInfoSerializer({
+#         'products':products,
+#         'count': len(products),
+#         'max_price': products.aggregate(max_price = Max('price'))['max_price']
+#     })
+#     return Response(serializer.data)
